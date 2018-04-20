60c8a883b274f811d50e35eb4d00a9f4f4565f62
AdrianStefan23 <adrian.cosmin23@gmail.com>
2018-04-20 18:13:51 +0300

    Simple /post

--- app.js
+++ app.js
@@ -3,17 +3,41 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const  cors = require('cors');
 const mongoose = require('mongoose');
+const util = require('util');
 
 
 const port = process.env.PORT || 3000;
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UsersDB');
+//mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/UsersDB');
+
+let key = {
+	accKey: "001a918c0dc345938b9ca76c2ab97574"
+};
 
 let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+let Bing = require('node-bing-api')({accKey: "001a918c0dc345938b9ca76c2ab97574"});
+
+/* 
+ * Simple post method for the images
+ */
+app.post('/images', (request, response) => {
+
+	let query = request.body.query;
+	Bing.images(query, (err, res, body) => {
+			response.send({
+				url: body.value[0].contentUrl
+			});
+		},
+		{
+			count: 1,
+			adult: 'Moderate'
+		}
+	);
+});
 
 app.get('/test', (req, res) => {
 	res.send('it works');
