eacc44be523518b1c56a6e21f65713044856b513
cochiletSerban <cochiletSerban@gmail.com>
2018-06-28 10:57:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/images', (request, response) => {
  */
 app.get('/jessica', (request, response) => {
 	console.log("got get on jessica");
-	let query = "Jessica Nigri";
+	let query = "bmw m3";
 	Bing.images(query, (err, res, body) => {
 			let urls = [];
 			let size = body.value.length;
